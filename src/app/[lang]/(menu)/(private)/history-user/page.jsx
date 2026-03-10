86f1bd45cf0210c84a306af1f991db5c3d41caae
Alan Edu Hernandez <alaneduh231@gmail.com>
2026-03-09 22:00:08 -0500

    feat: delete components

--- src/app/[lang]/(menu)/(private)/history-user/page.jsx
+++
@@ -1,9 +0,0 @@
-import { getDictionary } from '@/utils/getDictionary'
-import HistoryUser from '@/views/history-user'
-
-const HistoryUserApp = async ({ params }) => {
-  const dictionary = await getDictionary(params.lang)
-  return <HistoryUser dictionary={dictionary} lang={params.lang} />
-}
-
-export default HistoryUserApp
